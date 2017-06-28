7d5462b7481fd7b2f34a3fc3e4ea51bb23a57189
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-28 14:22:51 +0530

    Create NUMBER CHECKING1.C

---
+++ NUMBER CHECKING1.C
@@ -0,0 +1,20 @@
+main()
+{
+int n;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("\ the given number is positive");
+}
+else if(n<0)
+{
+printf("\ the given number is negative");
+}
+else
+{
+printf("\ the given number is zero");
+}
+getch();
+}
