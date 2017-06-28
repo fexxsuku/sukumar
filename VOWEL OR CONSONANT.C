562934661575623701aa48d67d9a3d71ba99ebbd
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-28 14:52:28 +0530

    Create VOWEL OR CONSONANT.C

---
+++ VOWEL OR CONSONANT.C
@@ -0,0 +1,16 @@
+void main()
+{
+char n;
+clrscr();
+printf("\n enter the character");
+scanf("%s",&n);
+if((n=='a')||(n=='e')||(n=='i')||(n=='o')||(n=='u')||(n=='A')||(n=='E')||(n=='I')||(n=='O')||(n=='U'))
+{
+printf("\n entered character is a vowel");
+}
+else
+{
+printf("\n entered character is a consonant");
+}
+getch();
+}
