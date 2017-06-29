625536adfdfe751c863ac62f8ebe35bc0be8bfb4
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-29 14:55:54 +0530

    Create sum of n number.c

---
+++ sum of n number.c
@@ -0,0 +1,13 @@
+void main()
+{
+int n,i,sum=0;
+clrscr();
+printf("\n enter the natural number to calculate sum ");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+getch();
+}
