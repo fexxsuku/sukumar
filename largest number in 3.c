89ca62b04912221891edc45ba81497befe2a0edc
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-29 14:14:30 +0530

    Create largest number in 3.c

---
+++ largest number in 3.c
@@ -0,0 +1,20 @@
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n enter the numbers abc ");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+printf("\n a is greater");
+}
+else if(b>c)
+{
+printf("\n b is greater");
+}
+else
+{
+printf("\n c is greater");
+}
+getch();
+}
