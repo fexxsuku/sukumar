0fdd528b890fad446192d092c144b87263be9a9d
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-29 14:02:21 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+void main()
+{
+char n;
+clrscr();
+printf("\n enter the character");
+scanf("%s",&n);
+if((n=='1')||(n=='2')||(n=='3')||(n=='4')||(n=='5')||(n=='6')||(n=='7')||(n=='8')||(n=='9')||(n=='0'))
+{
+printf("\n entered character is a number");
+}
+else
+{
+printf("\n entered character is a alphabet");
+}
+getch();
+}
