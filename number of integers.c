2d15ee3a95c6cb010ad4b3c139a3ef9cc18ebab7
fexxsuku <sukumarsukuskr@gmail.com>
2017-06-30 11:15:44 +0530

    Update number of integers.c

--- number of integers.c
+++ number of integers.c
@@ -1,1 +1,15 @@
 
+void main()
+{
+int n,count=0;
+clrscr();
+printf("\n enter the number ");
+scanf("%d",&n);
+while(n!=0)
+{
+n/=10;
+++count;
+}
+printf("\n tne number of integers are %d",count);
+getch();
+}
