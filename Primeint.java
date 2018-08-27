527434b77a15705af8274ff868b44b53ad91fe0f
fexxsuku <sukumarsukuskr@gmail.com>
2018-08-27 13:39:52 +0530

    Create Primeint.java

---
+++ Primeint.java
@@ -0,0 +1,24 @@
+public class Primeint {
+
+    public static void main(String[] args) {
+
+        int low = 8, high = 17;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
