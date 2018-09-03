ceb61f48bdc10acf59c7eb2d5f4cc650f7ee9d6f
fexxsuku <sukumarsukuskr@gmail.com>
2018-09-03 12:25:07 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,16 +4,13 @@ class Factorial
 {
 public static void main(String args[])
 {
-System.out.println("enter the number");
-Scanner sc=new Scanner(System.in);
-int num=sc.nextInt();
-if(num>1)
-{
-for(int i=0;<num2;i++)
-{
-int fact=fact*i;
-}
-}
-System.out.println("factorail of"+num "is"+fact);
+ int i,fact=1;  
+ System.out.println("enter nuymber");  
+ Scanner sc=new Scanner(System.in);
+ int number=sc.nextInt() ;  
+ for(i=1;i<=number;i++){    
+ fact=fact*i;    
+  }   
+  System.out.println("Factorial of "+number+" is: "+fact);   
 }
 }
