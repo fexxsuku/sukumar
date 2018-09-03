7d6902462d9bcdb34eb2fe5db74cfc1bb00aefc1
fexxsuku <sukumarsukuskr@gmail.com>
2018-09-03 12:17:58 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+class Factorial
+{
+public static void main(String args[])
+{
+System.out.println("enter the number");
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+if(num>1)
+{
+for(int i=0;<num2;i++)
+{
+int fact=fact*i;
+}
+}
+System.out.println("factorail of"+num "is"+fact);
+}
+}
