dfb14a167b58c63cd384b95097ea2b9b842b61b1
fexxsuku <sukumarsukuskr@gmail.com>
2019-06-13 11:37:26 +0530

    Create index matching.py

---
+++ index matching.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=[]
+array=list(map(int,input().split()))
+for x in range(0,n):
+    if(array[x]==x):
+        l.append(x)
+if(len(l)==0):
+    print("-1")
+l.sort()
+for x in l:
+    print(x,end=" ")
