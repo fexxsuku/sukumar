a2082da1442f6be91ec02c26c75bd5c7e969707d
fexxsuku <sukumarsukuskr@gmail.com>
2019-06-13 14:52:32 +0530

    Create fristunique.py

---
+++ fristunique.py
@@ -0,0 +1,11 @@
+a=int(input())
+n=list(map(int,input().split()))
+l=[]
+for x in range(a):
+    for i in range(x+1,len(n)):
+        if(n[i]==n[x]):
+          l.append(n[x])
+if (len(l)==0):
+    print("unique")
+else:
+    print(l[0])
