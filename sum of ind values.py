a86eb3e07a50fcd31badb8f475095ff97c7cab30
fexxsuku <sukumarsukuskr@gmail.com>
2019-06-13 14:44:14 +0530

    Create sum of ind values.py

---
+++ sum of ind values.py
@@ -0,0 +1,7 @@
+sn=int(input())
+sl=list(map(int,input().split()))
+for i in range(0,sn-2):
+    for j in range(i+1,sn-1):
+        for k in range(j+1,sn):
+            if(sl[i]+sl[j]==sl[k]):
+                print(sl[i],sl[j],sl[k])
