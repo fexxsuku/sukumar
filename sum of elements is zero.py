9e7077c57c7d83e3cb16bbf915e9902ab693d84a
fexxsuku <sukumarsukuskr@gmail.com>
2019-06-13 15:22:22 +0530

    Update sum of elements is zero.py

--- sum of elements is zero.py
+++ sum of elements is zero.py
@@ -1,7 +1,10 @@
-sn=int(input())
-sl=list(map(int,input().split()))
-for i in range(0,sn-2):
-    for j in range(i+1,sn-1):
-        for k in range(j+1,sn):
-            if(sl[i]+sl[j]==0):
-                print(sl[i],sl[j])
+e=int(input())
+x=list(map(int,input().split()))
+l=max(x)
+e,f=0,0
+for i in range(0,len(x)):
+  for j in range(i+1,len(x)):
+    if abs(x[i]+x[j])<l:
+      e,f=x[i],x[j]
+      l=abs(e+f)
+print(e,f)
