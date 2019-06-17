1a2d59b91f85ba9c6684b6751318abbe94e996ea
fexxsuku <sukumarsukuskr@gmail.com>
2019-06-17 10:50:45 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -3,6 +3,7 @@ flag=0
 for sv in sv1:
     if(sv=='a' or sv=='e' or sv=='i' or sv=='o' or sv=='u' or sv=='A' or sv=='E' or sv=='I' or sv=='O' or sv=='U'):
         flag=1
+        break
     else:
         flag=0
 if(flag==1):
