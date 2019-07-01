16394f82c48b95300f91dd957a3a037e3971337b
sukumar <sukumarsukuskr@gmail.com>
2019-07-01 17:30:16 +0530

    Update points in same line.py

--- points in same line.py
+++ points in same line.py
@@ -1,7 +1,7 @@
 s,v=map(int,input().split())
 s1,v1=map(int,input().split())
 s2,v2=map(int,input().split())
-if(s==s1 and s1==s2 and s2==s1 or v==v1 and v1==v2 and v2==v1):
+if((v1-v)/(s1-s)==(v2-v)/(s2-s)):
     print("yes")
 else:
     print("no")
